2529a1e0eda0f913e8dfe504d9f063119a2a1850
Denis Dueñas <denis.mejia@cit.hn>
2025-03-13 18:24:30 -0600

    Agregar archivos de proyecto.

---
+++ Module.Shared/Response/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.Shared.Response
+{
+    public record ApiResponse<T>(
+        bool Success,
+        int StatusCode,
+        T Data,
+        Dictionary<string, string[]> Errors = null
+    );
+}
