3cb605e98c797d94832901ffb85485deacc5376c
Denis Dueñas <denis.mejia@cit.hn>
2025-03-18 18:42:58 -0600

    feat<AuthControllers>: Implementa AuthController y RoleAuthorizeAttribute para control de acceso
    Cambios realizados:
    
    * Se agregó `AuthController` con endpoints para inicio de sesión, cierre de sesión y otras operaciones de autenticación.
    * Se implementó `RoleAuthorizeAttribute` para permitir el control de acceso basado en roles en los endpoints de la API.
    * Se actualizaron los endpoints existentes para utilizar `RoleAuthorizeAttribute` donde sea necesario.
    * Se agregaron validaciones y manejo de errores en `AuthController`.

---
+++ BistroPulseApi/Filters/RoleAuthorizeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Module.Shared.Response;
+
+namespace Module.Infrastructure.Filters
+{
+    public class RoleAuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly string[] _roles;
+        public RoleAuthorizeAttribute(params string[] roles)
+        {
+            _roles = roles;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+
+            if (!_roles.Any(role => user.IsInRole(role)))
+            {
+                context.Result = new UnauthorizedObjectResult(new ApiResponse<string>(
+                    Success: false,
+                    Message: "Forbidden",
+                    Data: null
+                ));
+                return;
+            }
+        }
+    }
+}
